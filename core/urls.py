3c74cdd468c32de029a7063b53994130f3603ed3
Aaron Alberto <aaronalbertotb@gmail.com>
2022-01-07 10:24:07 -0600

    Started the set up for the models accessible for admin, create superuser pending

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# import this in order to add the settings to manage media folder
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# we add the setting
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Product
+
+# Django makes the registration for us
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display =  ['name', 'slug']
+    # Django know what to do with the filters provided and will set the configuration needed for it
+    prepopulated_fields = {'slug' : ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price', 'in_stock', 'created', 'updated']
+    list_filter  = ['in_stock', 'is_active']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug' : ('title',)}
