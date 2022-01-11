9b16405ffbd28470e5c2d39cce093f6a0bac14e5
Aaron Alberto <aaronalbertotb@gmail.com>
2022-01-11 10:10:32 -0600

    Added a simple test for category model default name

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -8,10 +8,18 @@ class TestCategoriesModel(TestCase):
     def setUp(self):
         self.data1 = Category.objects.create(name='django', slug='django')
 
+    # is a good practice using a name that describes what the test is for
     def test_category_model_entry(self):
         """
         Test Category model data insertion/types/field attributes
         """
         data = self.data1
         self.assertTrue(isinstance(data, Category))
+        self.assertEqual(str(data), 'django')
+
+    def test_category_entry(self):
+        """
+        Test category model default name
+        """
+        data = self.data1
         self.assertEqual(str(data), 'django')
