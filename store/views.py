3ee09e80e94dee28b207d6f56c55c5b05e73cf1a
Aaron Alberto <aaronalbertotb@gmail.com>
2022-01-14 11:41:06 -0600

    Added some new categories, added the configuration into settings.py so Django is able to access the information for every page and finally passed the categories in base.html file so we can extend

--- store/views.py
+++ store/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 # Since we're working with models, we need to import them
 from .models import Category, Product
 
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
 # functions are basically the requests
 def all_products(request):
     # we run a query, SQL, to return all the products in the products table
@@ -11,3 +16,4 @@ def all_products(request):
     # let's save the data and make sure is available in the template
     # we are returning the template we want to use and the data we need
     return render(request, 'store/home.html', {'products': products})
+ 
