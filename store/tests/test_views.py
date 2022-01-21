852f033918949d5b030ae2512c7e800ba2763837
Aaron Alberto <aaronalbertotb@gmail.com>
2022-01-21 11:21:29 -0600

    Created a test for home page and product details; category test and others pending

---
+++ store/tests/test_views.py
@@ -0,0 +1,39 @@
+# provides a facility to skip test
+from unittest import skip
+
+from django.test import TestCase
+
+from django.contrib.auth.models import User
+from store.models import Category, Product
+from django.urls import reverse
+
+from django.test import Client
+
+
+# @skip("demonstrating skipping")
+# class TestSkip(TestCase):
+#     def test_skip_exmaple(self):
+#         pass
+
+
+class TestViewResponses(TestCase):
+    def setUp(self):
+        self.c = Client()
+        User.objects.create(username='admin')
+        Category.objects.create(name='django', slug='django')
+        Product.objects.create(category_id=1, tittle='django', created_by_id=1,
+                               slug='django', price='20.00', image='django')
+
+    def test_url_allowed_hosts(self):
+        """
+        Test allowed hosts
+        """
+        response = self.c.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_detail_url(self):
+        """
+        Test items response status
+        """
+        response = self.c.get(reverse('store:product_detail', args=['django']))
+        self.assertEqual(response.status_code, 200)
